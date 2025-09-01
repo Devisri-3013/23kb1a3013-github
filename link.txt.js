654f276d10d13d2a36e461bd662c93e0ad56af5b
Devisri-3013 <23kb1a3013@nbkrist.org>
2025-09-01 15:51:23 +0530

    Add files via upload

---
+++ link.txt.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link JS File Example</title>
+</head>
+<body>
+
+    <h1>Hello, World!</h1>
+
+    <!-- Link the JavaScript file -->
+    <script src="script.js"></script>
+
+</body>
+</html>
+
